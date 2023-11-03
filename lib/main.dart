4f16e6a486d089c82028c379dc90b2f4a16c7e99
Lucas Reis <lucasreis_9@hotmail.com>
2023-11-02 21:54:02 -0300

    Facebook_ui #1

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:facebook_ui/facebook_ui/facebook_ui.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,11 +14,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: ,
+      home: const FacebookUi(),
     );
   }
 }

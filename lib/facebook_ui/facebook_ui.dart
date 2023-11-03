75d4cc996812f6124ee4ba00178d3dfd7457e7b1
Lucas Reis <lucasreis_9@hotmail.com>
2023-11-03 14:38:35 -0300

    Facebook ui #2

--- lib/facebook_ui/facebook_ui.dart
+++ lib/facebook_ui/facebook_ui.dart
@@ -1,10 +1,38 @@
+import 'package:facebook_ui/icons/custom_icons_icons.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class FacebookUi extends StatelessWidget {
   const FacebookUi({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        systemOverlayStyle: const SystemUiOverlayStyle(
+          statusBarBrightness: Brightness.light,
+        ),
+        backgroundColor: Colors.white,
+        elevation: 0, //Remove a sombra do appBar
+        leadingWidth: 150,
+        leading: SvgPicture.asset(
+          'assets/logos/facebook.svg',
+          color: Colors.blueAccent,
+        ),
+        actions: [
+          Container(
+            width: 40,
+            height: 40,
+            decoration:
+                BoxDecoration(color: Colors.grey, shape: BoxShape.circle),
+            child: Icon(
+              Icons.search,
+              color: Colors.white,
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
